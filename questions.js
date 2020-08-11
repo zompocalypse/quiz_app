0666d221e5f934b227fe9f76c30d78bc00240f37
Jason Stankevich <jason.stankevich@gmail.com>
2020-08-11 16:08:37 -0400

    added use strict to js files

--- questions.js
+++ questions.js
@@ -1,28 +1,29 @@
+'use strict';
 const store = {
-    // 5 or more questions are required
-    questions: [
-      {
-        question: 'What color is broccoli?',
-        answers: [
-          'red',
-          'orange',
-          'pink',
-          'green'
-        ],
-        correctAnswer: 'green'
-      },
-      {
-        question: 'What is the current year?',
-        answers: [
-          '1970',
-          '2015',
-          '2019',
-          '2005'
-        ],
-        correctAnswer: '2019'
-      }
-    ],
-    quizStarted: false,
-    questionNumber: 0,
-    score: 0
-  };
+  // 5 or more questions are required
+  questions: [
+    {
+      question: 'What color is broccoli?',
+      answers: [
+        'red',
+        'orange',
+        'pink',
+        'green'
+      ],
+      correctAnswer: 'green'
+    },
+    {
+      question: 'What is the current year?',
+      answers: [
+        '1970',
+        '2015',
+        '2019',
+        '2005'
+      ],
+      correctAnswer: '2019'
+    }
+  ],
+  quizStarted: false,
+  questionNumber: 0,
+  score: 0
+};
